a0b4f4a95982a390bbe057b2b487040c33f28fcf
Harsh-Sharma-1 <85671456+Harsh-Sharma-1@users.noreply.github.com>
2022-01-22 22:25:32 +0530

    first commit

---
+++ components/HeaderItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const HeaderItem = ({Icon, title}) => {
+  return <div className='flex flex-col group cursor-pointer items-center w-12 sm:w-20 hover:text-white'>
+      <Icon className="h-8 mb-1 group-hover:animate-bounce"/>
+      <p className="opacity-0 group-hover:opacity-100  tracking-widest">{title}</p>
+  </div>;
+};
